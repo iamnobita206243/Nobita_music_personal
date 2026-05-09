6585c3e0a716f60b370d6df2843aefdde54c54c9
iamnobita206243 <nobitamusic001@gmail.com>
2026-05-09 17:05:35 +0530

    Update welcome.py

--- NobitaMusic/plugins/tools/welcome.py
+++ NobitaMusic/plugins/tools/welcome.py
@@ -173,7 +173,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
 **☉ ᴜ_ɴᴀᴍᴇ ⧽** @{user.username}
 **☉ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ⧽** {count}
 
-❖ ᴍᴀᴅᴇ ʙʏ  ➛ [𝚴 𝐎 𝐁 𝚰 𝐓 𝚲](https://t.me/II_YOUR_NOBITA_II)
+❖ ᴍᴀᴅᴇ ʙʏ  ➛ [𝚴 𝐎 𝐁 𝚰 𝐓 𝚲](https://t.me/II_NOBITA_DEFAULTERS_II)
 
 **▬▭▬▭▬▭▬▭▬▭▬▭▬▭**
 
