baf0d0449a3383b37fb6d2e1faf52fc67d544d70
iamnobita206243 <nobitamusic001@gmail.com>
2026-05-10 14:05:54 +0530

    Update ccgen.py

--- NobitaMusic/plugins/tools/ccgen.py
+++ NobitaMusic/plugins/tools/ccgen.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/DefaultersMusicRobot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/AuraMusiccRobot?startgroup=true"),
     ],
 ]
 
